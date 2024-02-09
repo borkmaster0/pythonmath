8979842f2c38dd52f56346b2b9190460b61ec7d4
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2024-02-08 20:45:44 -0500

    Merge

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
+import number
+import statistics
+import sets
+import controls
+
+dir()
+=======
 from number import *
 from controls import *
 from sets import *
 from stats import *
+>>>>>>> ba31bfe35cc329d564659c4d0c37e18a946b55bf
